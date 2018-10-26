93e2a5d1b1e37debae689a61405c1ac02a125b3a
guanw88 <guanw88@gmail.com>
2018-10-26 15:31:49 -0700

    Generate list of valid moves and potential moves

--- skeleton/knighttravails.rb
+++ skeleton/knighttravails.rb
@@ -1,9 +1,11 @@
 require_relative "./lib/00_tree_node.rb"
 
 class KnightPathFinder
+  SHIFTS = [[-2,-1], [-1,-2], [1,-2], [2,-1], [-2,1], [-1,2], [1,2], [2,1]]
 
   def initialize(pos)
     @root_node = PolyTreeNode.new(pos)
+    @visited_positions = [pos]
     build_move_tree
   end
 
@@ -11,4 +13,36 @@ class KnightPathFinder
 
   end
 
+  def new_move_positions(pos)
+    new_moves = KnightPathFinder.valid_moves(pos).reject {|move| @visited_positions.include?(move)}
+    @visited_positions += new_moves
+    return new_moves
+  end
+
+  def self.valid_moves(pos)
+    x , y = pos
+    moves = []
+
+    SHIFTS.each do |shift|
+      x_shift , y_shift = shift
+
+      moves << [x + x_shift, y + y_shift]
+    end
+    moves.select {|move| move.valid_move?}
+  end
+
+  # def valid_move?(pos)
+  #   x , y = pos
+  #   return false if x < 0 || x > 7 || y < 0 || y > 7
+  #   true
+  # end
+
+end
+
+class Array
+  def valid_move?
+    x , y = self
+    return false if x < 0 || x > 7 || y < 0 || y > 7
+    true
+  end
 end
