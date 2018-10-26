836c02aa80ee808ec9a3e026ff2b9ac9a8ec49e6
guanw88 <guanw88@gmail.com>
2018-10-26 11:57:43 -0700

    Create #parent= assignment method for PolyTreeNode class" git commit -m Create

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -10,4 +10,18 @@ class PolyTreeNode
     @children = []
   end
 
+  def parent=(parent_node)
+      old_parent = self.parent
+      new_parent = parent_node
+
+      if new_parent.nil?
+        old_parent.children.delete(self)
+      elsif new_parent.children.include?(self) == false
+        old_parent.children.delete(self) unless old_parent.nil?
+        new_parent.children << self
+      end
+
+      @parent = new_parent
+  end
+
 end
