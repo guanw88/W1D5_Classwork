43f35a7bfa8151ed5f46ae7e4fd545e5554b3863
guanw88 <guanw88@gmail.com>
2018-10-26 14:56:19 -0700

    Initialize KnightPathFinder class in knighttravails.rb

---
+++ skeleton/knighttravails.rb
@@ -0,0 +1,14 @@
+require_relative "./lib/00_tree_node.rb"
+
+class KnightPathFinder
+
+  def initialize(pos)
+    @root_node = PolyTreeNode.new(pos)
+    build_move_tree
+  end
+
+  def build_move_tree
+
+  end
+
+end
